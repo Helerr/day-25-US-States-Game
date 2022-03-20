c6c1537820967ff24053dac4e59c8be4a28cfbed
Neculai Alexandru <alleexx250595@gmail.com>
2022-03-20 21:21:05 +0200

    done with file saving for not guessed states

--- main.py
+++ main.py
@@ -9,18 +9,28 @@ data = pandas.read_csv("50_states.csv")
 screen.addshape(image)
 turtle.shape(image)
 game_in_progress = True
-
+guessed_states = []
 all_states = data["state"].to_list()
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
-
-if answer_state in all_states:
-    t = turtle.Turtle()
-    t.hideturtle()
-    t.penup()
-    state_data = data[data.state == answer_state]
-    t.goto(int(state_data.x), int(state_data.y))
-    t.write(answer_state)
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name?").title()
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
 # writer = turtle.Turtle()
 # writer.hideturtle()
 # writer.speed("fastest")
@@ -46,4 +56,3 @@ if answer_state in all_states:
 #         writer.goto(0, 0)
 #         writer.write("Congratulations! You guessed them all", move=False, align="center", font=("Arial", 30, ""))
 #         game_in_progress = False
-screen.exitonclick()
